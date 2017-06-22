7c7019cc4014bf3203f287e2961a8ff5fe47e1bf
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-06-22 06:30:46 -0400

    Added UIkit contrib theme and created NDSBS as sub-theme

---
+++ sites/default/themes/custom/ndsbs/js/ndsbs.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * Attaches behaviors for ndsbs.
+ */
+
+(function ($) {
+
+  'use strict';
+
+  Drupal.behaviors.NDSBSNavbar = {
+    attach: function () {
+    }
+  };
+})(jQuery);
--- sites/default/themes/custom/ndsbs_bootstrap/templates/custom/refund_payment_node_form.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @file
- * refund_payment_node_form.tpl.php
- */
-?>
-<?php print drupal_render_children($form); ?>
