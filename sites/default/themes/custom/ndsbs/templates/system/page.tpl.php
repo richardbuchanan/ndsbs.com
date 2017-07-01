30c629285ebc9d389e067f2f1ee56fbc3191bf2c
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-07-01 00:24:11 -0400

    Removed $page["content"] if frontpage

--- sites/default/themes/custom/ndsbs/templates/system/page.tpl.php
+++ sites/default/themes/custom/ndsbs/templates/system/page.tpl.php
@@ -158,8 +158,10 @@
         <ul class="action-links uk-subnav uk-subnav-pill"><?php print render($action_links); ?></ul>
       <?php endif; ?>
 
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
+      <?php if (!$is_front): ?>
+        <?php print render($page['content']); ?>
+        <?php print $feed_icons; ?>
+      <?php endif; ?>
     </div>
 
     <?php if ($page['sidebar_first']): ?>
