d5300fbcf5b23c04180841eaf6cacdd7dc60145c
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-07-12 20:03:54 -0400

    Major updates

---
+++ sites/dev.ndsbs.com/themes/custom/ndsbs_admin/template.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Process theme data for the NDSBS Admin theme.
+ */
+
+/**
+ * Load NDSBS Admin's include files for theme processing.
+ */
+include 'includes/preprocess.inc';
+include 'includes/process.inc';
+include 'includes/theme.inc';
+include 'includes/alter.inc';
