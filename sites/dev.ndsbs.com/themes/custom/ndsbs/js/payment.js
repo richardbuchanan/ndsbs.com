e93fbba21441954f4c24805696f8e1abb3ab4655
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-09-21 14:09:53 -0400

    Updated various staff dashboard templates and added Google AdWord conversion code to register and payment pages

--- sites/dev.ndsbs.com/themes/custom/ndsbs/js/payment.js
+++ sites/dev.ndsbs.com/themes/custom/ndsbs/js/payment.js
@@ -61,4 +61,29 @@ jQuery(document).ready(function ($) {
       return false;
     }
   });
+
+  /* <![CDATA[ */
+  goog_snippet_vars = function() {
+    var w = window;
+    w.google_conversion_id = 838747312;
+    w.google_conversion_label = "FdXdCP70gnUQsIn5jwM";
+    w.google_remarketing_only = false;
+  };
+
+  // DO NOT CHANGE THE CODE BELOW.
+  goog_report_conversion = function(url) {
+    goog_snippet_vars();
+    window.google_conversion_format = "3";
+    var opt = new Object();
+    opt.onload_callback = function() {
+      if (typeof(url) != 'undefined') {
+        window.location = url;
+      }
+    }
+    var conv_handler = window['google_trackConversion'];
+    if (typeof(conv_handler) == 'function') {
+      conv_handler(opt);
+    }
+  }
+  /* ]]> */
 });
