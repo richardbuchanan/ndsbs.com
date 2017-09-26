3215b7678c1ea3d70e2c9963ea59f7530b8ac923
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-09-26 19:10:53 -0400

    Updated payment refund request template

--- sites/ndsbs.com/themes/custom/ndsbs/templates/custom/list_all_client_payment_refund_request.tpl.php
+++ sites/ndsbs.com/themes/custom/ndsbs/templates/custom/list_all_client_payment_refund_request.tpl.php
@@ -28,73 +28,47 @@ drupal_add_js('misc/tableheader.js');
 <table class="uk-table uk-table-striped sticky-enabled">
   <thead>
   <tr class="bkg_b">
-    <th>S. No.</th>
-    <th>User Details</th>
-    <th>Refund for Service</th>
-    <th>Reason</th>
-    <th>Requesting Date</th>
-    <th>Action</th>
+    <th class="uk-text-nowrap">Client</th>
+    <th class="uk-text-nowrap">Refund for Service</th>
+    <th class="uk-text-nowrap">Reason</th>
+    <th class="uk-text-nowrap">Requesting Date</th>
+    <th class="uk-text-nowrap">Action</th>
   </tr>
   </thead>
   <tbody>
-  <?php
-  $i = 0;
-  $record_not_found = count($result);
-  foreach ($result as $rec) {
-    $user_info = user_load($rec->uid);
-    //echo '<pre>';
-    //print_r($user_info);
-    ?>
+  <?php $record_not_found = count($result); ?>
+  <?php foreach ($result as $rec): ?>
+    <?php $user_info = user_load($rec->uid); ?>
     <tr>
-      <td>
-        <?php print ++$i; ?>
-      </td>
-      <td>
+      <td class="uk-text-nowrap">
         <?php $name = $user_info->field_first_name['und'][0]['value'] . ' ' . $user_info->field_middle_name['und'][0]['value'] . ' ' . $user_info->field_last_name['und'][0]['value']; ?>
-        Service
-        User- <?php print l(t($name), 'user/' . $user_info->uid . '/edit'); ?>
+        <div><?php print l(t($name), 'user/' . $user_info->uid . '/edit'); ?></div>
       </td>
-      <td>
-        <?php
-        $term = taxonomy_term_load($rec->field_refund_for_service['und'][0]['tid']);
-        print $term->name;
-        ?>
+      <td class="uk-text-nowrap">
+        <?php $term = taxonomy_term_load($rec->field_refund_for_service['und'][0]['tid']); ?>
+        <div><?php print $term->name; ?></div>
       </td>
       <td>
         <?php print $rec->field_refund_reason['und'][0]['value']; ?>
       </td>
-      <td>
-        <?php print date('M d Y h:i A', $rec->created); ?>
+      <td class="uk-text-nowrap">
+        <div><?php print date('M d, Y @ h:i A', $rec->created); ?></div>
       </td>
-      <td>
-        <?php
-        if ($rec->field_request_status['und'][0]['value'] == 0) {
-          //  0 mean Open And 1 Mean Close
-          $options = array('attributes' => array('class' => 'closer_icon'));
-          print l(t('Close Request'), 'request/paymentrefund/update/' . $rec->nid, $options);
-        }
-        else {
-          print '<ul class="tr_actions">
-                                        <li class="close_icon">Request Closed</li>
-                                    </ul>';
-        }
-        ?>
+      <td class="uk-text-nowrap">
+        <?php if ($rec->field_request_status['und'][0]['value'] == 0): ?>
+          <div><?php print l(t('Close request'), 'request/paymentrefund/update/' . $rec->nid); ?></div>
+        <?php else: ?>
+          <div>Request closed</div>
+        <?php endif; ?>
       </td>
     </tr>
-    <?php
-  }
-  ?>
-  <?php
-  if ($record_not_found <= 0) {
-    ?>
+  <?php endforeach; ?>
+  <?php if ($record_not_found <= 0): ?>
     <tr>
-      <td class="txt_ac" colspan="6">Record not found.</td>
+      <td colspan="5">Record not found.</td>
     </tr>
-    <?php
-  }
-  ?>
+  <?php endif; ?>
   </tbody>
 </table>
-<?php
-$total = 10;
-print $output = theme('pager', array('quantity' => $total)); ?>
+<?php $total = 10; ?>
+<?php print $output = theme('pager', array('quantity' => $total)); ?>
