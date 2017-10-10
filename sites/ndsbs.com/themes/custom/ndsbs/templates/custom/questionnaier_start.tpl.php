bcc9cd88f738fc3ca3a8ad96e664685f1e13790c
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-10-10 12:32:41 -0400

    Reverted questionnaire start page to before multiple assessments functionality was added

--- sites/ndsbs.com/themes/custom/ndsbs/templates/custom/questionnaier_start.tpl.php
+++ sites/ndsbs.com/themes/custom/ndsbs/templates/custom/questionnaier_start.tpl.php
@@ -3,6 +3,6 @@ questionnaire_move();
 
 include 'stepsheader.tpl.php';
 ?>
-<?php print drupal_render($assessment_select_form); ?>
 <?php print $questionnaire_instructions; ?>
-<?php print drupal_render($questionnaire_form); ?>
+<?php print drupal_render($form); ?>
+
