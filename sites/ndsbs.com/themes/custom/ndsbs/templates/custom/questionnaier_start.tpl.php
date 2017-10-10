9114c83491991d54b806d5a8d4120892afcc1119
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-10-10 13:50:32 -0400

    Updated how client questionnaire questions are generated

--- sites/ndsbs.com/themes/custom/ndsbs/templates/custom/questionnaier_start.tpl.php
+++ sites/ndsbs.com/themes/custom/ndsbs/templates/custom/questionnaier_start.tpl.php
@@ -1,8 +1,4 @@
-<?php
-questionnaire_move();
-
-include 'stepsheader.tpl.php';
-?>
+<?php include 'stepsheader.tpl.php'; ?>
+<?php print drupal_render($assessment_select_form); ?>
 <?php print $questionnaire_instructions; ?>
-<?php print drupal_render($form); ?>
-
+<?php print drupal_render($questionnaire_form); ?>
