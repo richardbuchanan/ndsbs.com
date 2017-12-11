3cfe8da2d68e4b411af094c5195afbf3589fd199
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-12-11 13:25:48 -0500

    Added payment_confirmation field to table {users_transactions} and updated the information rendered on payment confirmation page

--- sites/all/modules/custom/ndsbs_payment/payment_confirmation.tpl.php
+++ sites/all/modules/custom/ndsbs_payment/payment_confirmation.tpl.php
@@ -18,63 +18,65 @@ foreach ($transactions as $transaction) {
 $assessment_node = node_load($transactions[0]->nid);
 $assessment_user = user_load($transactions[0]->uid);
 $assessment_title = $assessment_node->title;
+$transaction_cost = $transactions[0]->cost;
 $rush_cost = !(empty($_SESSION['ndsbs_payment']['rush_amount'])) ? $_SESSION['ndsbs_payment']['rush_amount'] : number_format(0, 2);
+$total_cost = $transaction_cost + $rush_cost;
 
 $payment['order_id'] = $transactions[0]->order_id;
 $payment['user_email'] = $assessment_user->mail;
 $payment['assessment'] = $assessment_title;
 $payment['cost'] = $transactions[0]->cost;
 $payment['rush_cost'] = number_format($rush_cost, 2);
-$payment['total_cost'] = number_format($transactions[0]->cost + $rush_cost, 2);
+$payment['total_cost'] = number_format($total_cost, 2);
 $payment['misc_service'] = $misc_service;
+
+$payment_confirmation_status = ndsbs_payment_get_payment_confirmation_status($user->uid, $transactions[0]->transaction_id);
+
 ?>
-<h2>Order Details</h2>
-<div class="uk-alert-success" uk-alert>
-  <p>Payment has been successfully processed.</p>
-</div>
-<table class="uk-table uk-table-striped sticky-enabled">
-  <thead>
-    <tr>
-      <th>Assessment</th>
-      <th>Amount</th>
-    </tr>
-  </thead>
-  <tbody>
-  <tr class="assessment-row">
-    <td class="assessment-title"><?php print $payment['assessment']; ?></td>
-    <td class="assessment-total">$ <?php print $payment['cost']; ?></td>
-  </tr>
-  <tr class="rush-service-row">
-    <td class="rush-service-title text-right">Rush Service Amount:</td>
-    <td class="rush-service-amount">$ <?php print $payment['rush_cost']; ?></td>
-  </tr>
-  <tr class="cart-total-row">
-    <td class="cart-total-title text-right">Total Amount:</td>
-    <td class="cart-total-amount">$ <?php print $payment['total_cost']; ?></td>
-  </tr>
-  </tbody>
-</table>
-
-<?php $text = !$payment['misc_service'] ? t('Begin my assessment') : t('Return to dashboard'); ?>
-<a href="/questionnaire/start/trans" class="uk-button uk-button-primary"><?php print $text; ?></a>
-
-<!-- Google Code for NDSBS Conversion Page -->
-<script type="text/javascript">
-  /* <![CDATA[ */
-  var google_conversion_id = 1017904011;
-  var google_conversion_language = "en";
-  var google_conversion_format = "3";
-  var google_conversion_color = "ffffff";
-  var google_conversion_label = "PLjyCJWdoQwQi_ev5QM";
-  var google_conversion_value = <?php print $payment['total_cost']; ?>;
-  var google_conversion_currency = "USD";
-  var google_remarketing_only = false;
-  /* ]]> */
-</script>
-<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-</script>
-<noscript>
-  <div style="display:none;">
-    <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/1017904011/?value=<?php print $payment['total_cost']; ?>&amp;currency_code=USD&amp;label=PLjyCJWdoQwQi_ev5QM&amp;guid=ON&amp;script=0"/>
+<?php if ($payment_confirmation_status): ?>
+  <h2>Order Details</h2>
+  <div class="uk-alert-warning" uk-alert>
+    <p>Payment has already been processed.</p>
   </div>
-</noscript>
+
+  <a href="/questionnaire/start/trans" class="uk-button uk-button-primary"><?php print t('Return to dashboard'); ?></a>
+<?php else: ?>
+  <h2>Order Details</h2>
+  <div class="uk-alert-success" uk-alert>
+    <p>Payment has been successfully processed.</p>
+  </div>
+  <table class="uk-table uk-table-striped sticky-enabled">
+    <thead>
+      <tr>
+        <th>Assessment</th>
+        <th>Amount</th>
+      </tr>
+    </thead>
+    <tbody>
+    <tr class="assessment-row">
+      <td class="assessment-title"><?php print $payment['assessment']; ?></td>
+      <td class="assessment-total">$ <?php print $payment['cost']; ?></td>
+    </tr>
+    <tr class="rush-service-row">
+      <td class="rush-service-title text-right">Rush Service Amount:</td>
+      <td class="rush-service-amount">$ <?php print $payment['rush_cost']; ?></td>
+    </tr>
+    <tr class="cart-total-row">
+      <td class="cart-total-title text-right">Total Amount:</td>
+      <td class="cart-total-amount">$ <?php print $payment['total_cost']; ?></td>
+    </tr>
+    </tbody>
+  </table>
+
+  <?php $url = url('/questionnaire/start/trans', array(
+    'query' => array(
+      'payment_confirmation' => '1',
+      'uid' => $user->uid,
+      'tid' => $transactions[0]->transaction_id,
+    ),
+  )); ?>
+  <?php $text = !$payment['misc_service'] ? t('Begin my assessment') : t('Return to dashboard'); ?>
+  <a href="<?php print $url; ?>" class="uk-button uk-button-primary"><?php print $text; ?></a>
+
+  <!-- INSERT GOOGLE CONVERSION CODE HERE! -->
+<?php endif; ?>
--- sites/dev.ndsbs.com/themes/contrib/uikit/src/UIkit.php
+++ sites/dev.ndsbs.com/themes/contrib/uikit/src/UIkit.php
@@ -19,7 +19,7 @@ class UIkit {
    *
    * @var string
    */
-  const UIKIT_LIBRARY_VERSION = '3.0.0-beta.34';
+  const UIKIT_LIBRARY_VERSION = '3.0.0-beta.35';
 
   /**
    * The Drupal project page for the UIkit base theme.
